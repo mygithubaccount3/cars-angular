0453adb33596b6f88aa9a1730ee536451df6388e
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2021-12-23 21:27:00 +0200

    improve markup

--- src/app/owner-detail/owner-detail.component.ts
+++ src/app/owner-detail/owner-detail.component.ts
@@ -28,7 +28,8 @@ export class OwnerDetailComponent {
     '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>';
   ARROW_BACK_ICON: string =
     '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>';
-
+  CREATE_ICON: string =
+    '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/></svg>';
   get cars(): FormArray {
     return this.carForm.get('cars') as FormArray;
   }
@@ -84,6 +85,11 @@ export class OwnerDetailComponent {
     iconRegistry.addSvgIconLiteral(
       'arrow_back',
       sanitizer.bypassSecurityTrustHtml(this.ARROW_BACK_ICON)
+    );
+
+    iconRegistry.addSvgIconLiteral(
+      'add_circle',
+      sanitizer.bypassSecurityTrustHtml(this.CREATE_ICON)
     );
 
     if (this.mode === 'view' || this.mode === 'edit') {
